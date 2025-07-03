1860e0bd7e729a86859f3ed9db1d05c55d684193
Shahd Shares <nossiershahd32@gmail.com>
2025-07-03 10:11:00 +0300

    Add files via upload

---
+++ 800-1100/800/Xenia_and_Ringroad.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    long long n, m;
+    cin >> n >> m;
+    vector <long long> v(m);
+    for (int i = 0; i < m; i++) {
+        cin >> v[i];
+    }
+    long long current = 1, time = 0;
+    for (long long i = 0; i < m; i++) {
+        if (v[i] >= current) {
+            time += v[i] - current;
+        }
+        else {
+            time += n - current + v[i];
+        }
+        current = v[i];
+    }
+    cout << time << '\n';
+}
