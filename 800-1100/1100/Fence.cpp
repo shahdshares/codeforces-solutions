3bf59949b9f8eed99777e83b2d94eccad892b417
Shahd Shares <nossiershahd32@gmail.com>
2025-07-05 17:14:21 +0300

    Add files via upload

---
+++ 800-1100/1100/Fence.cpp
@@ -0,0 +1,29 @@
+// https://codeforces.com/problemset/problem/363/B
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector <int> v(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> v[i];
+    }
+    vector <long long> prfx(n + 1 , 0);
+    for (int i = 1; i <= n; i++) {
+        prfx[i] = prfx[i - 1] + v[i];
+    }
+    int min_indx = INT_MAX;
+    long long min_sm = LLONG_MAX;
+    for (int l = 0, r = k; r <= n; l++, r++) {
+        if (prfx[r] - prfx[l] < min_sm) {
+            min_sm = prfx[r] - prfx[l];
+            min_indx = l;
+        }
+    }
+    cout << min_indx + 1 << '\n';
+}
