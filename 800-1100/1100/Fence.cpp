5d6a4871ea9d10b83b1bf3909d5bec94b72b8533
Shahd Shares <nossiershahd32@gmail.com>
2025-07-05 18:49:14 +0300

    Update Fence.cpp

--- 800-1100/1100/Fence.cpp
+++ 800-1100/1100/Fence.cpp
@@ -27,3 +27,34 @@ int main() {
     }
     cout << min_indx + 1 << '\n';
 }
+
+// ------------------------------------------------------
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector <int> v(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> v[i];
+    }
+    vector <long long> prfx(n + 1 , 0);
+    for (int i = 1; i <= n; i++) {
+        prfx[i] = prfx[i - 1] + v[i];
+    }
+    int min_indx = INT_MAX;
+    long long min_sm = LLONG_MAX;
+    for (int l = 0; l <= n - k; l++) {
+        int r = l + k;
+        if (prfx[r] - prfx[l] < min_sm) {
+            min_sm = prfx[r] - prfx[l];
+            min_indx = l;
+        }
+    }
+    cout << min_indx + 1 << '\n';
+}
