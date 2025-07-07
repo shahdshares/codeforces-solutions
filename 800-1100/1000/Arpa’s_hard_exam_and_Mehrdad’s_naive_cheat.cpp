01cf8d9a5cfaab4d9595271e38cc5afff28b8ae1
Shahd Shares <nossiershahd32@gmail.com>
2025-07-07 11:27:19 +0300

    Add files via upload

---
+++ 800-1100/1000/Arpa’s_hard_exam_and_Mehrdad’s_naive_cheat.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    long long n = pow(1378 , 4);
+    cout << n << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t --) {
+        solve();    
+    }
+}
