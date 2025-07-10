a27a30ad99459cd7c734a61e53edc2b3e9444d35
Shahd Shares <nossiershahd32@gmail.com>
2025-07-10 23:50:00 +0300

    Add files via upload

---
+++ 800-1100/800/To_My_Critics.py
@@ -0,0 +1,3 @@
+# https://codeforces.com/problemset/problem/1850/A
+
+for i in range (int(input())): digits = sorted(list(map(int, input().split()))) ; print("YES" if digits[0] + digits[1] >= 10 else "YES" if digits[1] + digits[2] >= 10 else "NO")    
