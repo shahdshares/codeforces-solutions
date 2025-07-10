40ba7ea517f23daafb456486a9e90a21773337d6
Shahd Shares <nossiershahd32@gmail.com>
2025-07-10 19:23:10 +0300

    Add files via upload

---
+++ 800-1100/1000/Jzzhu _and_Children.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath> 
+#include <vector>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    int n, m, x;
+    cin >> n >> m;
+    deque <pair<int , int>> dq;
+    for (int i = 1; i <= n; i++) {
+        cin >> x;
+        dq.push_back({i , x});
+    }
+    while (dq.size() > 1) {
+        if (m >= dq.front().second) {
+            dq.pop_front();
+        }
+        else {
+            dq.front().second -= m;
+            dq.push_back(dq.front());
+            dq.pop_front();
+        }
+    }
+    cout << dq.front().first << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+}
