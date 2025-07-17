b94116f9296d5e44803c02e51d99c8425dae82c4
Shahd Shares <nossiershahd32@gmail.com>
2025-07-17 20:28:05 +0300

    Add files via upload

---
+++ 800-1100/800/In_Search_of_an_Easy_Problem.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+void solve() {
+    int n, x, c = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        c ++;
+    }
+    if (c > 0) cout << "EASY\n";
+    else cout << "HARD\n";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1; 
+    // cin >> t;
+    while(t--) {
+        solve();
+    }
+}
